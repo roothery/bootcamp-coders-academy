6830bbe3a07309082cf333e9127f939719832a7e
roothery <roothery@gmail.com>
2020-11-21 14:00:26 -0400

    Construção e Migração do EF Core

---
+++ backend/CodersAcademy/CodersAcademy.API/Model/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodersAcademy.API.Model
+{
+    public class Album
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String Backdrop { get; set; }
+        public String Band { get; set; }
+
+        public IList<Music> Musics { get; set; }
+    }
+}
